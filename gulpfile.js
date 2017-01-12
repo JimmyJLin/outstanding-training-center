fb174e595d48f816c908c82fd1a75f2cde4f966c
Jimmy Lin <usmcjimmy@gmail.com>
2017-01-12 16:06:29 -0500

    basic layout with nav bar and footer

---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+'use strict';
+
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const $ = require('gulp-load-plugins')()
+const nodemon = require('gulp-nodemon');
+const livereload = require('gulp-livereload');
+
+
+// SASS css
+gulp.task('css', function(){
+  return gulp.src('app/sass/main.scss', { style: 'expanded'})
+    .pipe($.sourcemaps.init())
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      broswers: [ 'last 2 versions' ],
+      cascade: false
+    }))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('./public/css'))
+    .pipe($.livereload())
+})
+gulp.task('css:build', ['css'], function(){
+  return gulp.src('app/sass/main.scss')
+    .pipe($.rev())
+    .pipe(gulp.dest('./public/css'))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest('./public/css'))
+})
+
+// flexslider css
+gulp.task('flexslidercss', function(){
+  gulp.src('app/sass/flexslider.css')
+      .pipe(gulp.dest('./public/css'))
+      .pipe($.livereload())
+})
+gulp.task('flexslidercss:build', ['flexslidercss'], function(){
+  return gulp.src('app/css/flexslider.css')
+    .pipe($.rev())
+    .pipe(gulp.dest('./public/css'))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest('./public/css'))
+})
+
+// javascript
+gulp.task('scripts', function(){
+  gulp.src('app/js/main.js')
+      .pipe(gulp.dest('./public/js'))
+      .pipe($.livereload())
+})
+gulp.task('scripts:build', ['scripts'], function(){
+  return gulp.src('app/js/main.js')
+    .pipe($.rev())
+    .pipe(gulp.dest('./public/js'))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest('./public/js'))
+})
+
+// jquery
+gulp.task('jquery', function(){
+  gulp.src('app/js/jquery.js')
+      .pipe(gulp.dest('./public/js'))
+      .pipe($.livereload())
+})
+gulp.task('jquery:build', ['jquery'], function(){
+  return gulp.src('app/js/main.js')
+    .pipe($.rev())
+    .pipe(gulp.dest('./public/js'))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest('./public/js'))
+})
+
+// flexslider
+gulp.task('flexslider', function(){
+  gulp.src('app/js/jquery.flexslider.js')
+      .pipe(gulp.dest('./public/js'))
+      .pipe($.livereload())
+})
+gulp.task('flexslider:build', ['flexslider'], function(){
+  return gulp.src('app/js/jquery.flexslider.js')
+    .pipe($.rev())
+    .pipe(gulp.dest('./public/js'))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest('./public/js'))
+})
+
+// images
+gulp.task('images', function() {
+  gulp.src([
+    './app/images/**/*'
+  ])
+  .pipe(gulp.dest('./public/images'))
+  .pipe($.livereload())
+})
+gulp.task('images:build', ['images'], function() {
+  return gulp.src('app/images/**/*')
+             .pipe($.rev())
+             .pipe(gulp.dest('./public/images'))
+             .pipe($.rev.manifest())
+             .pipe(gulp.dest('./public/images'))
+})
+
+// ejs
+gulp.task('ejs', function(){
+  return gulp.src('views/**/*.ejs')
+  .pipe($.livereload())
+})
+
+// gulp watch tasks
+gulp.task('watch', function(){
+  livereload.listen();
+  gulp.watch('app/sass/**/*.scss', ['css'])
+  gulp.watch('app/sass/flexslider.css', ['flexslidercss'])
+  gulp.watch('app/js/main.js', ['scripts'])
+  gulp.watch('app/js/jquery.js', ['jquery'])
+  gulp.watch('app/js/jquery.flexslider.js', ['flexslider'])
+  gulp.watch('app/images/**/*', ['images'])
+  gulp.watch('views/**/*.ejs', ['ejs'])
+})
+
+// gulp serve
+gulp.task('server', function(){
+  nodemon({
+    'script': 'server.js',
+    'ignore': 'app/js/*.js'
+  })
+})
+
+gulp.task('serve', ['server', 'watch'])
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const engine = require('ejs-mate');
+
+/* app setting */
+const port = process.env.PORT || 3000;
+const app = express();
+const server = app.listen(port);
+
+
+/* express server setting  */
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('views', './views');
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+/* routes */
+const mainRoutes = require('./routes/main');
+
+app.use('/', mainRoutes);
+
+app.get('/about_us', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('pages/contact');
+});
