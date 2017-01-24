7ca3c5f595f97740700333f1a4fd1f5726a7e5b7
Jimmy Lin <usmcjimmy@gmail.com>
2017-01-23 19:38:19 -0500

    completed front page

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,77 @@
-$(function(){
+$(() => {
   console.log('script file loaded');
+  // $('#est_earnings').on('hover', () => {
+  //   console.log('loading est_earnings');
+  // });
+  $('#est_earnings').hover(() => {
 
-  // Toggle Buttons - add class active if clicked and remove if click again
-  $('.button.toggle').on("click", function(){
-    if($(this).hasClass('selected')){
-      $(this).removeClass('selected').text('Vote')
-    } else {
-      $(this).addClass('selected').text('Voted')
-    }
+    const tuitionValue = document.getElementById('tuition');
+
+    const costValue = document.getElementById('cost');
+
+    const weeklyValue = document.getElementById('weekly');
+
+    const annualValue = document.getElementById('annual');
+
+    const start = 0;
+    const tutionEnd = 1800;
+    const costEnd = 500;
+    const weeklyEnd = 2500;
+    const annualEnd = 13000;
+
+    const duration = 500; // In milliseconds (divide by 1000 to get seconds).
+    const framerate = 5;    // In milliseconds (divide by 1000 to get seconds).
+
+    // toAddTuition
+    const toAddTuition = ((tutionEnd - start) * framerate) / duration;
+
+    const tuitionInterval = setInterval(() => {
+      const currentValue = parseFloat(tuitionValue.innerHTML);
+
+      if (currentValue >= tutionEnd) {
+        clearInterval(tuitionInterval);
+        return;
+      }
+      tuitionValue.innerHTML = (!isNaN(currentValue) === true ? currentValue + toAddTuition : toAddTuition);
+    }, framerate);
+
+    // toAddCost
+    const toAddCost = ((costEnd - start) * framerate) / duration;
+
+    const costInterval = setInterval(() => {
+      const currentValue = parseFloat(costValue.innerHTML);
+
+      if (currentValue >= costEnd) {
+        clearInterval(tuitionInterval);
+        return;
+      }
+      costValue.innerHTML = (!isNaN(currentValue) === true ? currentValue + toAddCost : toAddCost);
+    }, framerate);
+
+    // toAddWeekly
+    const toAddWeekly = ((weeklyEnd - start) * framerate) / duration;
+
+    const weeklyInterval = setInterval(() => {
+      const currentValue = parseFloat(weeklyValue.innerHTML);
+
+      if (currentValue >= weeklyEnd) {
+        clearInterval(tuitionInterval);
+        return;
+      }
+      weeklyValue.innerHTML = (!isNaN(currentValue) === true ? currentValue + toAddWeekly : toAddWeekly);
+    }, framerate);
+
+    // toAddAnnual
+    const toAddAnnual = ((annualEnd - start) * framerate) / duration;
+
+    const annualInterval = setInterval(() => {
+      const currentValue = parseFloat(annualValue.innerHTML);
+
+      if (currentValue >= annualEnd) {
+        clearInterval(tuitionInterval);
+        return;
+      }
+      annualValue.innerHTML = (!isNaN(currentValue) === true ? currentValue + toAddAnnual : toAddAnnual);
+    }, framerate);
   });
 });
